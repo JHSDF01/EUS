3c4d10e313a97fbe7acf131fc083bf1cf8322c73
JHSDF01 <kyuujitujieitai@gmail.com>
2018-05-18 12:42:19 +0900

    関数追加、車両交換と描画と車両移動

---
+++ cars.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+def entry_cars(houe,sec,unyo_number,fuji,kama):
+    car=[]
+    if car is None:
+        car[0]= fuji
+        car[1]= kama
+    return car
+
+def add_cars(ud, car,fuji, kama):
+    #単行に出庫車を連結する場合
+    if ud = 'd':
+
+
+def parge_cars(ud, car,fuji, kama):
+    #重連を開放して入庫する場合
+    if ud = 'd':
+    
+
+
+def change_cars(ud, car,fuji, kama):
+    #重連の前に2両を連結し、後ろ2両を開放する場合
+    if ud = 'd':
---
+++ draw_train.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+def draw_train(hour,sec):
+    
+    fujisawa
+    kugenuma
+    enoshima
+    minegahara
+    inamuragasaki
+    gokurakuji
+    hase
+    kamakura
+    if sec % 12 == 0:
+        #藤沢待機
+
+    if sec % 12 == 6:
+        #峰が原待機
+
+    if sec % 12 == 7:
+        #上り極楽寺
+
+    if sec % 12 == 2:
+        #下り極楽寺
+
+    down = 
+    up = 
