d0ec26e780a633cd541c938e1adb8f82a8d0e2f9
JHSDF01 <kyuujitujieitai@gmail.com>
2018-05-18 16:25:56 +0900

    車両交換を定義

--- cars.py
+++ cars.py
@@ -8,17 +8,33 @@ def entry_cars(houe,sec,unyo_number,fuji,kama):
         car[1]= kama
     return car
 
-def add_cars(ud, car,fuji, kama):
+def add_cars(ud, car, train):
     #単行に出庫車を連結する場合
     if ud = 'd':
+        train[1] = car
+    else:
+        train[0] = train[1]
+        train[0] = car
+        pass
 
 
-def parge_cars(ud, car,fuji, kama):
+def parge_cars(ud, train, car):
     #重連を開放して入庫する場合
     if ud = 'd':
-    
+        train[0] = train[1]
+        del train[1]
+    else:
+        del train[1]
+        pass
 
 
-def change_cars(ud, car,fuji, kama):
+def change_cars(ud, car, train):
     #重連の前に2両を連結し、後ろ2両を開放する場合
     if ud = 'd':
+        train
+        train[0] = train[1]
+        train[1] = car
+    else:
+        train[1] = train[0]
+        train[0] = car
+        pass
