1cb4c0dd8d46e58e77462be2680ae2ac4b5edc3d
JHSDF01 <kyuujitujieitai@gmail.com>
2018-05-19 00:13:39 +0900

    矢印追加

--- draw_train.py
+++ draw_train.py
@@ -26,7 +26,7 @@ def draw_train(hour,sec):
     un4 = "2003+1502"
     un5 = " 501+ 305"
     un6 = "1101+1001"
-    stationid =[0 for i in range(31)]
+    stationid =[0 for i in range(32)]
 
     locate_train(stationid, un1, 0)
     locate_train(stationid, un2, 5)
@@ -44,38 +44,41 @@ def draw_train(hour,sec):
     location = "藤沢　石上　柳小　鵠沼　湘南　江ノ　腰越　鎌高　峰原　七里　稲村　極楽　長谷　由比　和田　鎌倉"
     track_s =  "                  ----        ----              ----        ----        ----              ----"
     track_m =  "----------------------------------------------------------------------------------------------"
-    for i in range(0,15):
+    for i in range(0,16):
+        print(i)
         if stationid[i] != 0:
             down += str(stationid[i]) 
             down += " "
         else:
             if i != 0 and stationid[i-1] != 0:
-                down += " "
+                down += "  "
             else:            
                 down += "      " 
 
         if stationid[i] != 0:
-            downicon += "  |   "
+            downicon += "  |>  "
         else:
             downicon += "      "
     
-    for i in reversed(range(16,31)):
+    for i in reversed(range(16,32)):
+        print(i)
         if stationid[i] != 0:
             up += str(stationid[i])
             up += " "
             
         else:
-            if i >= 31 and stationid[i+1] != 0:
-                up += " "
+            if i < 31 and stationid[i+1] != 0:
+                up += "  "
             else:
                 up += "      " 
 
         if stationid[i] != 0:
-            upicon += "  |   "
+            upicon += " <|   "
         else:
             upicon += "      "
 
-    print( "\n\n" + hour + "時" + sec + "分現在の" + "江ノ電運用" + "\n\n")
+    print("Enoden Unyo Simurator test")
+    print( "\n\n" + hour + "時" + sec + "分現在の" + "江ノ電車両位置" + "\n\n")
     print(down)
     print(downicon)
     print(track_s)
@@ -83,6 +86,11 @@ def draw_train(hour,sec):
     print(track_m)
     print(upicon)
     print(up)
+    print("\n遅延など反映できない場合があります。\n")
+    print("\n")
+    print("\nこれはテスト画面です。実際の運用ではありません。\n")
+
+
     return
 
-draw_train('9','45')
+draw_train('9','48')
