916374007a9228c4b210c15dfe421e52bae12ba2
JHSDF01 <kyuujitujieitai@gmail.com>
2018-05-18 16:53:08 +0900

    運用の移動を定義する

--- cars.py
+++ cars.py
@@ -1,40 +1,47 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-def entry_cars(houe,sec,unyo_number,fuji,kama):
-    car=[]
+# Un6 = entry_cars(9, 24, 305, 1001)
+#　みたいな書き方をしていく
+
+
+
+def entry_cars(hour,sec,fuji,kama):
+    train=[]
     if car is None:
-        car[0]= fuji
-        car[1]= kama
-    return car
+        train[0]= fuji
+        train[1]= kama
+    return train
 
 def add_cars(ud, car, train):
     #単行に出庫車を連結する場合
-    if ud = 'd':
-        train[1] = car
+    if ud == 'd':
+        train.append(car)
     else:
-        train[0] = train[1]
-        train[0] = car
-        pass
+        #train[0] = train[1]
+        #train[0] = car
+        train[0],train[1] = car, train[0]
+    return train
 
 
 def parge_cars(ud, train, car):
     #重連を開放して入庫する場合
-    if ud = 'd':
+    if ud == 'd':
         train[0] = train[1]
         del train[1]
     else:
         del train[1]
-        pass
+    return train
 
 
 def change_cars(ud, car, train):
     #重連の前に2両を連結し、後ろ2両を開放する場合
-    if ud = 'd':
-        train
-        train[0] = train[1]
-        train[1] = car
+    if ud == 'd':
+        #train[0] = train[1]
+        #train[1] = car
+        train[0],train[1] = train[1], car
     else:
-        train[1] = train[0]
-        train[0] = car
-        pass
+        #train[1] = train[0]
+        #train[0] = car
+        train[0],train[1] = car, train[0]
+    return train
--- move_train.py
+++ move_train.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-def move_train(hour,sec):
+#Unyoは配列名で、Un1からun6まである。
+
+def set_train(hour, sec, un_number, station):
+    if hour == 5 && sec == 43:
+        station = un_number 
+        #stationにunyoをセットする。5時43分に運用1が出庫するので、この場合は極楽寺駅に[1]が格納される
+        un_number = 
+        #un1 に　train をセット
+
+
+def move_train(hour,sec,un1, un2, un3, un4, un5, un6):
     #駅とホームにナンバーを割り当てて配列化する
     #藤沢基準
     station = []
-    fujisawa 
-    kugenuma
-    enoshima
-    minegahara
-    inamuragasaki
-    gokurakuji
-    hase
-    kamakura
+    fujisawa = "---------"
+    kugenuma = "---------"
+    enoshima = "---------"
+    minegahara = "---------"
+    inamuragasaki = "---------"
+    gokurakuji = "---------"
+    hase = "---------"
+    kamakura = "---------"
     if sec % 12 == 0:
         #藤沢待機
+
 
     if sec % 12 == 6:
         #峰が原待機
