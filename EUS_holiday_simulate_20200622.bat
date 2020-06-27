25b50d51346cead43e135afa48180796bc86eab8
JHSDF <30502682+JHSDF01@users.noreply.github.com>
2020-06-27 16:02:20 +0900

    シミュレーション機能増強
    数日後の運用シミュレーションを実装。検車区アルゴリズムの検証中。

--- EUS_holiday_simulate_20200622.bat
+++ EUS_holiday_simulate_20200622.bat
@@ -1,4 +1,27 @@
-py .\EUS_main.py 24 24 0.1 3
+rem 日中から入庫シミュレーション　日曜
+py .\EUS_main.py 23 24 0.1 3
+
+rem 朝から入庫シミュレーション　月曜
 copy save\depot_save.json  save\depot_load.json
-py .\EUS_main.py 11 11 0.1 0
+py .\EUS_main.py 23 24 0.1 0
+
+rem 朝から入庫シミュレーション　火曜
+copy save\depot_save.json  save\depot_load.json
+py .\EUS_main.py 23 24 0.1 0
+
+rem 朝から入庫シミュレーション　水曜
+copy save\depot_save.json  save\depot_load.json
+py .\EUS_main.py 23 24 0.1 0
+
+rem 朝から入庫シミュレーション　木曜
+copy save\depot_save.json  save\depot_load.json
+py .\EUS_main.py 23 24 0.1 0
+
+rem 朝から入庫シミュレーション　金曜
+copy save\depot_save.json  save\depot_load.json
+py .\EUS_main.py 23 24 0.1 0
+
+rem 朝から入庫シミュレーション　土曜
+copy save\depot_save.json  save\depot_load.json
+py .\EUS_main.py 11 12 0.1 1
 pause
